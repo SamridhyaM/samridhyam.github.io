40ec36f2b66cf4b6240cecfe18da0acc44ec7cdc
SamridhyaM <samridhyamukherjee@gmail.com>
2023-11-27 18:10:17 -0500

    Index switched from html to js

---
+++ index.js
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> Homepage </title>
+    </head>
+    <body>
+        <div>
+            <p>
+                Hello this is the landing page.
+            </p>
+        </div>
+    </body>
+</html>
