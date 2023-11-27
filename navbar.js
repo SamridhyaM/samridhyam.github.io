752c0223012d7aafde57eeb1cb0818186eb3c871
SamridhyaM <samridhyamukherjee@gmail.com>
2023-11-27 18:08:35 -0500

    Navbar creation

---
+++ navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      {/* Your Navbar JSX code goes here */}
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
